159a3eecacd161b186677b9b186cadc47f155a02
Donald Adu-Poku <donald.adu@gmail.com>
2025-04-11 14:59:11 +0000

    add string tests for timeframe.

--- shared/timeframe_test.go
+++ shared/timeframe_test.go
@@ -13,3 +13,29 @@ func TestNewYorkTime(t *testing.T) {
 	assert.Equal(t, now.Location().String(), "America/New_York")
 	assert.Equal(t, now.Location().String(), loc.String())
 }
+
+func TestTimeframeString(t *testing.T) {
+	tests := []struct {
+		name      string
+		timeframe Timeframe
+		want      string
+	}{
+		{
+			"One Hour",
+			OneHour,
+			"1H",
+		},
+		{
+			"Five Minute",
+			FiveMinute,
+			"5m",
+		},
+	}
+
+	for _, test := range tests {
+		str := test.timeframe.String()
+		if str != test.want {
+			t.Errorf("%s: expected %v, got %v", test.name, test.want, str)
+		}
+	}
+}
